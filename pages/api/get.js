716c06532afdb11b6d592c701e314baf9ec858b2
Shubham Patil <shubhampatilsd@gmail.com>
2022-01-16 13:08:12 -0800

    added reverse showing of time

--- pages/api/get.js
+++ pages/api/get.js
@@ -3,8 +3,16 @@ const { Deta } = require("deta");
 export async function getPosts() {
   const deta = Deta(process.env.project_key);
   const db = deta.Base("marks");
-  const data = await db.fetch();
-  return data.items;
+
+  let res = await db.fetch();
+  let allItems = res.items;
+
+  // continue fetching until last is not seen
+  while (res.last) {
+    res = await db.fetch({}, { last: res.last });
+    allItems = allItems.concat(res.items);
+  }
+  return allItems.reverse();
 }
 
 export default async (req, res) => {
